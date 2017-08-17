b09eaef32b1e369c5f101b1dd012b40140d42da6
Stephany Bacelis <sttephyblz@gmail.com>
2017-08-17 16:17:04 -0600

    Input Data

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -32,15 +32,37 @@ var budgetController = (function() {
 // UI Controller
 var UIController = (function() {
 
-  //Some code
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+
+  return {
+    getInput: function(){
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function(){
+      return DOMstrings;
+    }
+  };
 
 })();
 
 //Global App Controller
 var controller = (function(budgetCtrl, UICtrl){
 
+  var DOM = UICtrl.getDOMstrings();
+
   var ctrlAddItem = function(){
     // 1. Get the filed input data
+    var input = UICtrl.getInput();
+    console.log(input);
 
     // 2. Add the item to the budget controller
 
@@ -49,10 +71,9 @@ var controller = (function(budgetCtrl, UICtrl){
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
-    console.log('Woo');
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event){
     if(event.keyCode === 13 || event.which === 13){
