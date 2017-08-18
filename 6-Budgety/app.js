ba91c1eb106c273e528c1a964bccc635dff66446
Stephany Bacelis <sttephyblz@gmail.com>
2017-08-18 11:40:57 -0600

    Creating an Initialization Function

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -57,13 +57,25 @@ var UIController = (function() {
 //Global App Controller
 var controller = (function(budgetCtrl, UICtrl){
 
-  var DOM = UICtrl.getDOMstrings();
+  var setupEventListeners = function(){
+
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+      if(event.keyCode === 13 || event.which === 13){
+        ctrlAddItem();
+      }
+    });
+  };
+
+
 
   var ctrlAddItem = function(){
     // 1. Get the filed input data
     var input = UICtrl.getInput();
-    console.log(input);
-
+    
     // 2. Add the item to the budget controller
 
     // 3. Add the item to the UI
@@ -71,14 +83,16 @@ var controller = (function(budgetCtrl, UICtrl){
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
-  }
+  };
 
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function(event){
-    if(event.keyCode === 13 || event.which === 13){
-      ctrlAddItem();
+  return {
+    init: function(){
+      console.log('Application');
+      setupEventListeners();
     }
-  });
+  };
 
 })(budgetController, UIController);
+
+
+controller.init();
